2fa3c76114b68bb2a1a38d1d448875e21f8bede7
tanjilul <tanjilulanwar@gmail.com>
2020-06-02 09:01:44 +0600

    Updates

---
+++ precache-manifest.71a896dc63f6ed218bdbfe856906d0ce.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54e7ab3a6feaa1a197de99beed3a771",
+    "url": "/RoboShowcase/index.html"
+  },
+  {
+    "revision": "c2a5ae13551778163201",
+    "url": "/RoboShowcase/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "93f97a00a580a366b3c9",
+    "url": "/RoboShowcase/static/css/main.41f31c93.chunk.css"
+  },
+  {
+    "revision": "c2a5ae13551778163201",
+    "url": "/RoboShowcase/static/js/2.692a01d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoboShowcase/static/js/2.692a01d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93f97a00a580a366b3c9",
+    "url": "/RoboShowcase/static/js/main.4ab160d0.chunk.js"
+  },
+  {
+    "revision": "e76e2f6ac2ff0f4e3fe7",
+    "url": "/RoboShowcase/static/js/runtime-main.d10545a2.js"
+  },
+  {
+    "revision": "7eeda46c5dd5cf5e3e5218592b764772",
+    "url": "/RoboShowcase/static/media/Kotoba-l2zq.7eeda46c.woff"
+  },
+  {
+    "revision": "584b607f5c2ff075429dc0e7b8d142ef",
+    "url": "/RoboShowcase/static/media/loading.584b607f.gif"
+  }
+]);
